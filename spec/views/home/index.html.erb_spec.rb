16349d4e246dcf699974ae5c69f0e8a8a4fd0a76
shibuya <mio.shibuya@trictrac.co.jp>
2022-01-07 02:48:22 +0900

    add rubocop

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "home/index.html.erb", type: :view do
-  it "displays the header" do
+RSpec.describe 'home/index.html.erb', type: :view do
+  it 'displays the header' do
     # rspec-railsがrails7に公式未対応のためと思われるが、
     # renderにtemplateを明示的に指定しないと見つからない。
     # https://github.com/rspec/rspec-rails/pull/2521
     render template: 'home/index'
 
-    expect(rendered).to include "Welcome"
+    expect(rendered).to include 'Welcome'
   end
 end

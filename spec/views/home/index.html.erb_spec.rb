5b34eee559cdecb1e3be28f243c82834bae8b009
shibuya <mio.shibuya@trictrac.co.jp>
2022-01-07 02:24:26 +0900

    add spec

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "displays the header" do
+    # rspec-railsがrails7に公式未対応のためと思われるが、
+    # renderにtemplateを明示的に指定しないと見つからない。
+    # https://github.com/rspec/rspec-rails/pull/2521
+    render template: 'home/index'
+
+    expect(rendered).to include "Welcome"
+  end
 end
